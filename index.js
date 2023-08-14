afe617f00edb48f89c05690153eee39122e8e47f
Alphable <327110986@qq.com>
2023-08-14 15:28:04 +0800

    connected MongoDB.

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 // 通过require函数导入了Express.js模块
 const express = require('express')
 const cors = require('cors')
+require('dotenv').config()
+// console.log('loaded env variables.')
+const Note = require('./models/note')
 // 创建了一个Express应用程序实例
 const app = express()
 // 中间件是可以用来处理 request 和 response 对象的函数
@@ -8,9 +11,10 @@ const app = express()
 // 这对于处理 POST、PUT 和 PATCH 请求等需要从请求体中提取数据的情况非常有用!
 app.use(express.json())// 使用中间件
 app.use(cors())
-app.use(express.static('build'))
+app.use(express.static('build'))//连接前端build包。每当express收到一个HTTP GET请求时，它将首先检查build目录中是否包含一个与请求地址相对应的文件。如果找到了正确的文件，express将返回它
 
 const requestLogger = (request, response, next) => {
+  console.log('-----request logger------')
   console.log('Method:', request.method)
   console.log('Path:  ', request.path)
   console.log('Body:  ', JSON.stringify(request.body))
@@ -41,24 +45,32 @@ let notes = [
       }
 ]
 
-const generateId = () => {
+
+
+
+/* const generateId = () => {
   const maxID = notes.length >0 ?
   Math.max(...notes.map(note => {return note.id}))
   : 0
   return maxID +1
-}
+} */
 app.get('/', (request, response) => {
-  // respond with data
+  // respond with data if BUILD(compressed files of frontend) doesn't have corresponding file
   response.send('<h1>Hello World!</h1>')
 })
 
 app.get('/api/notes', (request, response) => {
-  // respond with data
-  response.json(notes)
+  // 回应硬编码的notes
+  // response.json(notes)
+
+  // 改为从MongoDB的Note集合取数据
+  Note.find({}).then(notes => {
+    response.json(notes)
+  })
 })
 
 app.get('/api/notes/:id', (request, response) =>{
-  const id = Number(request.params.id) //字符串转成数字
+/*   const id = Number(request.params.id) //字符串转成数字
   const note = notes.find(note => {
     return(note.id === id)})
     if(note) {
@@ -66,7 +78,20 @@ app.get('/api/notes/:id', (request, response) =>{
     }else{
       // .end():quickly end the response without any data
       response.status(404).end()
-    }
+    } */
+    Note.findById(request.params.id)
+    .then(note => {
+      if (note) {
+        response.json(note);
+      } else {
+        console.log('not founded.')
+        response.status(404).end();
+      }
+    })
+    .catch(error => {
+      response.status(500).json(error);
+    });
+  
 })
 
 app.delete('/api/notes/:id', (request, response) => {
@@ -81,7 +106,7 @@ app.post('/api/notes', (request, response) => {
   if (!body.content){
     return response.status(400).json({error: 'content missing'})
   }
-  const note = {
+/*   const note = {
     content: body.content,
     date: new Date().toISOString(),
     important: body.important || false,
@@ -89,7 +114,18 @@ app.post('/api/notes', (request, response) => {
   }
 
   notes = notes.concat(note)
-  response.json(note)
+  response.json(note) */
+
+  // 用Note构造函数创建
+  const note = new Note({
+    content: body.content,
+    important:body.important || false,
+    date: new Date()
+  })
+  note.save().then(savedNote => {
+    response.json(savedNote) //响应中送回的数据用toJSON方法创建的格式化版本。
+    console.log('note saved!')
+  })
 
 })
 
@@ -113,7 +149,11 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint)
 
-const PORT = process.env.PORT || 3001
+// 使用环境变量(by heroku)端口或本地端口
+const PORT = process.env.PORT
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
+
+
