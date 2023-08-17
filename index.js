2749694d66d2ecbf47c775f4fe79c8b61280a2d0
Alphable <327110986@qq.com>
2023-08-17 11:31:21 +0800

    add ESLINT

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
 // 通过require函数导入了Express.js模块
 const express = require('express')
-// require('dotenv').config()
+require('dotenv').config()
 const cors = require('cors')
 
 // console.log('loaded env variables.')
@@ -15,12 +17,12 @@ app.use(cors())// 必须在satic之前
 app.use(express.static('build'))//连接前端build包。每当express收到一个HTTP GET请求时，它将首先检查build目录中是否包含一个与请求地址相对应的文件。如果找到了正确的文件，express将返回它
 
 const requestLogger = (request, response, next) => {
-  console.log('-----request logger------')
-  console.log('Method:', request.method)
-  console.log('Path:  ', request.path)
-  console.log('Body:  ', JSON.stringify(request.body))
-  console.log('---')
-  next()
+	console.log('-----request logger------')
+	console.log('Method:', request.method)
+	console.log('Path:  ', request.path)
+	console.log('Body:  ', JSON.stringify(request.body))
+	console.log('---')
+	next()
 }
 
 app.use(requestLogger)
@@ -28,24 +30,24 @@ app.use(requestLogger)
 
 
 let notes = [
-    {
-        id: 1,
-        content: "HTML is kind of easy",
-        date: "2022-05-30T17:30:31.098Z",
-        important: true
-      },
-      {
-        id: 2,
-        content: "Browser can execute only Javascript",
-        date: "2022-05-30T18:39:34.091Z",
-        important: false
-      },
-      {
-        id: 3,
-        content: "GET and POST are the most important methods of HTTP protocol",
-        date: "2022-05-30T19:20:14.298Z",
-        important: true
-      }
+	{
+		id: 1,
+		content: 'HTML is kind of easy',
+		date: '2022-05-30T17:30:31.098Z',
+		important: true
+	},
+	{
+		id: 2,
+		content: 'Browser can execute only Javascript',
+		date: '2022-05-30T18:39:34.091Z',
+		important: false
+	},
+	{
+		id: 3,
+		content: 'GET and POST are the most important methods of HTTP protocol',
+		date: '2022-05-30T19:20:14.298Z',
+		important: true
+	}
 ]
 
 
@@ -58,22 +60,22 @@ let notes = [
   return maxID +1
 } */
 app.get('/', (request, response) => {
-  // respond with data if BUILD(compressed files of frontend) doesn't have corresponding file
-  response.send('<h1>Hello World!</h1>')
+	// respond with data if BUILD(compressed files of frontend) doesn't have corresponding file
+	response.send('<h1>Hello World!</h1>')
 })
 
 app.get('/api/notes', (request, response) => {
-  // 回应硬编码的notes
-  // response.json(notes)
-console.log('getting 3001/api/notes')
-  // 改为从MongoDB的Note集合取数据
-  Note.find({}).then(notes => {
-    response.json(notes)
-    console.log('got 3001/api/notes')
-  })
+	// 回应硬编码的notes
+	// response.json(notes)
+	console.log('getting 3001/api/notes')
+	// 改为从MongoDB的Note集合取数据
+	Note.find({}).then(notes => {
+		response.json(notes)
+		console.log('got 3001/api/notes')
+	})
 })
 
-app.get('/api/notes/:id', (request, response, next) =>{
+app.get('/api/notes/:id', (request, response, next) => {
 /*   const id = Number(request.params.id) //字符串转成数字
   const note = notes.find(note => {
     return(note.id === id)})
@@ -83,40 +85,39 @@ app.get('/api/notes/:id', (request, response, next) =>{
       // .end():quickly end the response without any data
       response.status(404).end()
     } */
-    Note.findById(request.params.id)
-    .then(note => {
-      if (note) {
-        response.json(note);
-      } else {
-        console.log('not founded.')
-        response.status(404).end();
-      }
-    })
-    .catch(error => {
-// 当您遇到错误并希望将控制权传递给错误处理中间件,进入错误处理中间件
-      next(error)
-    });
-  
+	Note.findById(request.params.id)
+		.then(note => {
+			if (note) {
+				response.json(note)
+			} else {
+				console.log('not founded.')
+				response.status(404).end()
+			}
+		})
+		.catch(error => {
+			// 当您遇到错误并希望将控制权传递给错误处理中间件,进入错误处理中间件
+			next(error)
+		})
 })
 
-app.delete('/api/notes/:id', (request, response) => {
+app.delete('/api/notes/:id', (request, response, next) => {
 /*   const id = Number(request.params.id)
   // 实现删除=保留ID不等于传入ID的所有笔记
   notes = notes.filter(note => note.id !== id)
   response.status(204).end() */
-  Note.findByIdAndRemove(request.params.id)
-  .then(result => {
-    response.status(204).end()
-  })
-  .catch(error => next(error))
+	Note.findByIdAndRemove(request.params.id)
+		.then(result => {
+			response.status(204).end()
+		})
+		.catch(error => next(error))
 })
 
 app.post('/api/notes', (request, response, next) => {
-  const body = request.body
-  if (!body.content){
-    return response.status(400).json({error: 'content missing'})
-  }
-/*   const note = {
+	const body = request.body
+	if (!body.content){
+		return response.status(400).json({ error: 'content missing' })
+	}
+	/*   const note = {
     content: body.content,
     date: new Date().toISOString(),
     important: body.important || false,
@@ -126,17 +127,17 @@ app.post('/api/notes', (request, response, next) => {
   notes = notes.concat(note)
   response.json(note) */
 
-  // 用Note构造函数创建
-  const note = new Note({
-    content: body.content,
-    important:body.important || false,
-    date: new Date()
-  })
-  note.save().then(savedNote => {
-    response.json(savedNote) //响应中送回的数据用toJSON方法创建的格式化版本。
-    console.log('note saved!')
-  })
-  .catch(error => next(error))
+	// 用Note构造函数创建
+	const note = new Note({
+		content: body.content,
+		important:body.important || false,
+		date: new Date()
+	})
+	note.save().then(savedNote => {
+		response.json(savedNote) //响应中送回的数据用toJSON方法创建的格式化版本。
+		console.log('note saved!')
+	})
+		.catch(error => next(error))
 
 })
 
@@ -154,34 +155,34 @@ app.put('/api/notes/:id',(request, response, next) => {
   }else{
     response.status(404).end()
   } */
-  const body = request.body
-  const note = {
-    content: body.content,
-    important: body.important
-  }
-  // findOneAndUpdate被执行时，默认不运行验证
-  Note.findByIdAndUpdate(request.params.id, note, {new: true, runValidators: true})
-  .then(
-    updatedNote => {
-      response.json(updatedNote)
-    }
-  ).catch(error => next(error))
+	const body = request.body
+	const note = {
+		content: body.content,
+		important: body.important
+	}
+	// findOneAndUpdate被执行时，默认不运行验证.manually set property runValidators
+	Note.findByIdAndUpdate(request.params.id, note, { new: true, runValidators: true })
+		.then(
+			updatedNote => {
+				response.json(updatedNote)
+			}
+		).catch(error => next(error))
 })
 // 对前面都无法处理的路由进行处理
 const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' })
+	response.status(404).send({ error: 'unknown endpoint' })
 }
 app.use(unknownEndpoint)
 
 // 所有错误处理中间件
 const errorHandler = (error, request, response, next) => {
-  console.log(error.message)
-  if (error.name === 'CastError'){
-    return response.status(400).send({error: 'malformatted id'})
-  }else if (error.name === 'ValidationError'){
-    return response.status(400).json({error: error.message})
-  }
-  next(error) //将请求传递给下一个中间件或路由处理程序
+	console.log(error.message)
+	if (error.name === 'CastError'){
+		return response.status(400).send({ error: 'malformatted id' })
+	}else if (error.name === 'ValidationError'){
+		return response.status(400).json({ error: error.message })
+	}
+	next(error) //将请求传递给下一个中间件或路由处理程序
 }
 app.use(errorHandler)
 
@@ -189,7 +190,7 @@ app.use(errorHandler)
 // 使用环境变量(by heroku)端口或本地端口
 const PORT = process.env.PORT
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+	console.log(`Server running on port ${PORT}`)
 })
 
 
--- models/note.js
+++ models/note.js
@@ -1,36 +1,38 @@
+/* eslint-disable no-unused-vars */
 const mongoose = require('mongoose')
+// eslint-disable-next-line no-undef
 const url = process.env.MONGODB_URL
 console.log('connecting to', url)
 
 mongoose.connect(url).then(result => {
-    console.log('connected to MongoDB')
+	console.log('connected to MongoDB')
 })
-.catch(error => {
-    console.log('error connecting to MongoDB: ', error.message)
-})
+	.catch(error => {
+		console.log('error connecting to MongoDB: ', error.message)
+	})
 
 const noteSchema = new mongoose.Schema({
-  content:{
-    type: String,
-    // minLength和required验证器是内置的
-    minLength: 5,
-    required: true
-  },
-  date: {
-    type: Date,
-    required: true
-  },
-  important: Boolean
+	content:{
+		type: String,
+		// minLength和required验证器是内置的
+		minLength: 5,
+		required: true
+	},
+	date: {
+		type: Date,
+		required: true
+	},
+	important: Boolean
 })
 
 // response.json(xxx)这个将响应转成json格式的函数会自动调用以下toJSON
 // 自定义响应的格式
 noteSchema.set('toJSON', {
-  transform: (document, returnedObj) => { //document是数据库的一行，returnedObj是想返回给用户的响应。
-    returnedObj.id = returnedObj._id.toString()
-    delete returnedObj._id
-    delete returnedObj.__v
-  }
+	transform: (document, returnedObj) => { //document是数据库的一行，returnedObj是想返回给用户的响应。
+		returnedObj.id = returnedObj._id.toString()
+		delete returnedObj._id
+		delete returnedObj.__v
+	}
 })
 // 模块的公共接口
 module.exports = mongoose.model('Note', noteSchema)  // 设为Note模型
